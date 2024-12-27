f2d3685ccd0ff27b68493215e58e422a23739cb9
Elain Polakova <S00250500@atu.ie>
2024-12-27 17:29:26 +0000

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+// Handle Add to Cart
+function addToCart(itemId) {
+    console.log(`Item ${itemId} added to cart.`);
+    window.location.href = 'cart.html';
+}
+
+// Handle Add to Favourites
+function addToFavourites(itemId) {
+    console.log(`Item ${itemId} added to favourites.`);
+    window.location.href = 'favourites.html';
+}
